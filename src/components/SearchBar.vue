a9c46937bd31e441cd40671ac250b5fecfd1d118
Jake Morris <Jakemorrishello@gmail.com>
2022-12-01 12:47:30 +0100

    search bar

--- src/components/SearchBar.vue
+++ src/components/SearchBar.vue
@@ -1,34 +1,69 @@
 <template>
-    <div class="relative w-50 ml-auto mr-20 items-center flex">
+    <div class="relative w-50 ml-auto items-center flex">
         <div class="absolute left-2 i-carbon-search"></div>
         <input ref="searchBar" class="rounded-md overflow-hidden pr-2 py-2 dark:bg-sky-700 pl-8" v-model="input" />
         <transition name="slide">
-            <button v-if="input" @click="search"
-                class="absolute -right-20 px-2 py-1 bg-sky-600 border-1 border-sky-600 rounded-md text-white">
-                search
-            </button>
+            <div @click="search" v-auto-animate v-if="results.length"
+                class="z-1 absolute top-12 px-2 py-2 border-0 border-white rounded-md text-white w-50 text-left overflow-hidden transition-all gap-1 flex flex-col"
+                :class="input ? 'shadow-2xl bg-white' : 'bg-transparent'">
+                <div v-for="(result, index) in results" :key="index" class="cursor-pointer transition-all text-black pl-3 hover:bg-sky-100 rounded-md py-1" @click="goToLink(result)">
+                    {{ formatResult(result) }}
+                </div>
+            </div>
         </transition>
     </div>
 </template>
 
 <script setup lang="ts">
 import { MaybeComputedRef, onKeyPressed } from '@vueuse/core';
+import { Customer, Ticket } from '~/types';
 
-const searchBar : MaybeComputedRef<EventTarget> = ref(null);
+const searchBar: MaybeComputedRef<EventTarget> = ref(null);
 console.log(searchBar)
 const { input } = useSearch(searchBar, { autofocus: true })
 const { push } = useRouter()
 
+
 onKeyPressed('Enter', () => {
-    console.log("search-bar: ", searchBar)
-    search()
+    // search()
 }, {
     target: searchBar
 })
 
-const search = () => {
-    if (input.value)
-        push(`/app/search?input=${encodeURIComponent(input.value)}`)
+// const search = () => {
+//     if (input.value)
+//         push(`/app/search?input=${encodeURIComponent(input.value)}`)
+// }
+
+const { tickets } = useTicketStore();
+const { customers } = useCustomerStore();
+
+const results = computed(() => {
+    if (!input.value) return []
+    const ticketsResults = tickets.filter(ticket => ticket.title.toLowerCase().includes(input.value.toLowerCase()))
+    const customersResults = customers.filter(customer => customer.name.toLowerCase().includes(input.value.toLowerCase()))
+    return [...ticketsResults, ...customersResults].slice(0, 5)
+})
+
+const formatResult = (result: Ticket | Customer) => {
+    if (isTicket(result)) {
+        return `Ticket ${result.id}: ${result.title}`
+    } else {
+        return `Customer ${result.id}: ${result.name}`
+    }
+}
+
+const isTicket = (result: Ticket | Customer): result is Ticket => {
+    return (result as Ticket).articles !== undefined
+}
+
+const goToLink = (result: Ticket | Customer) => {
+    if (isTicket(result)) {
+        push(`/app/ticket/${result.id}`)
+    } else {
+        push(`/app/customer/${result.id}`)
+    }
+    input.value = ''
 }
 </script>
 
@@ -39,12 +74,12 @@ const search = () => {
 }
 
 .slide-enter-from {
-    transform: translateX(-5px);
+    transform: translateY(-5px);
     opacity: 0;
 }
 
 .slide-leave-to {
-    transform: translateX(-5px);
+    transform: translateY(-5px);
     opacity: 0;
 }
 </style>
